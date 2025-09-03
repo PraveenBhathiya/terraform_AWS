76d77256a8da53f5dfdd73954a0c64cf2441d9a7
Praveen Bhathiya <praveenbandara421@gmail.com>
2025-09-03 15:29:09 +0530

    added AZ in main.tf in vpc module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,6 +7,8 @@ resource "aws_subnet" "public_subnet" {
     vpc_id = aws_vpc.main_vpc.id
     cidr_block = var.public_subnet_cidr
     map_public_ip_on_launch = true
+    availability_zone = "ap-south-1a"
+
     tags = { Name = "PublicSubnet" }  
 }
 
